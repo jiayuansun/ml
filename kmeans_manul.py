5e21c8955f4b12421590859570d2c2b4a2eb5301
jy <sunjiayuan25@gmail.com>
2018-12-27 23:51:19 -0500

    k means manual

--- kmeans_manul.py
+++ kmeans_manul.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import warnings
 from matplotlib import style
 from collections import Counter
+import random
+import pandas as pd 
+
 style.use('fivethirtyeight')
 # plot1=[1,3]
 # plot2=[2,5]
@@ -35,12 +38,45 @@ def k_nearest_neighbors(data,predict,k=3):
             distances.append([euclidean_distance,group])
     votes=[i[1] for i in sorted(distances)[:k]] #i is the group
     print(votes)
-    print(Counter(votes).most_common(1))
+    #print(Counter(votes).most_common(1))
     vote_result=Counter(votes).most_common(1)[0][0]
     return vote_result
 
-result=k_nearest_neighbors(dataset,new_features,k=3)
+# result=k_nearest_neighbors(dataset,new_features,k=3)
 
-print(result)
-plt.scatter(new_features[0],new_features[1],s=100)
-plt.show()
+# print(result)
+# plt.scatter(new_features[0],new_features[1],s=100)
+# plt.show()
+
+df=pd.read_csv("C:/Users/jisun/Desktop/python/ml_dataset/breast-cancer-wisconsin.data.txt")
+df.replace('?',-99999,inplace=True)
+df.drop(['id'],1,inplace=True)
+
+full_data=df.astype(float).values.tolist()
+print(full_data[:5])
+random.shuffle(full_data)
+print('#'*20)
+print(full_data[:5])
+
+test_size=0.2
+train_set={2:[],4:[]} #2 is bengih 
+test_set={2:[],4:[]}
+train_data=full_data[:-int(test_size*len(full_data))]
+test_data=full_data[-int(test_size*len(full_data)):]
+
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1]) #the i[-1] is the degree of the cancer b or last stage, the i[-1:] is the all the other element
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct=0
+total=0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote=k_nearest_neighbors(train_set,data,k=5)
+        if group==vote:
+            correct+=1
+        total+=1
+print('Acurracy:',correct/total)
